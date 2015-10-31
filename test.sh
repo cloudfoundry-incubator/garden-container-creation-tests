7e10ac4ad3d65f178f03da43c0d635b01f9eaaeb
Tammer Saleh <tsaleh@gmail.com>
2015-10-31 14:45:25 +0000

    Cleaner output and logging

--- test.sh
+++ test.sh
@@ -2,8 +2,12 @@
 
 set -xe
 
-exec > test.out
-exec 2>&1
+if [ $# != 2 ]; then
+  echo "Usage: $(basename $0) SUFFIX NUM"
+  echo
+  echo "Will deploy four apps, scale them to 15 instances total, and repeat NUM times"
+  exit 2
+fi
 
 suffix=$1
 numtimes=$2
@@ -23,18 +27,22 @@ touch results.csv
 
 for i in `seq $numtimes`; do
 
-  cf push westley-$suffix -p assets/apps/westley -m 128M 
-  cf push max-$suffix -p assets/apps/max -m 512M
-  cf push buttercup-$suffix -p assets/apps/princess -m 1024M 
-  cf push humperdink-$suffix -p assets/apps/humperdink -m 128M 
+  echo "Pushing apps..."
+
+  cf push westley-$suffix -p assets/apps/westley -m 128M  &> $suffix.out
+  cf push max-$suffix -p assets/apps/max -m 512M &> $suffix.out
+  cf push buttercup-$suffix -p assets/apps/princess -m 1024M  &> $suffix.out
+  cf push humperdink-$suffix -p assets/apps/humperdink -m 128M  &> $suffix.out
 
   scale_up_start_time=$(date +%s)
 
   for i in `seq 20` ; do
-    cf scale humperdink-$suffix -i $(( 2*$i ))
-    cf scale westley-$suffix -i $(( 8*$i ))
-    cf scale max-$suffix -i $(( 4*$i ))
-    cf scale buttercup-$suffix -i $(( 1*$i ))
+    echo "Scaling apps, round $i..."
+
+    cf scale humperdink-$suffix -i $(( 2*$i )) &> $suffix.out
+    cf scale westley-$suffix -i $(( 8*$i )) &> $suffix.out
+    cf scale max-$suffix -i $(( 4*$i )) &> $suffix.out
+    cf scale buttercup-$suffix -i $(( 1*$i )) &> $suffix.out
 
     check_state westley-$suffix $(( 8*$i ))
     check_state max-$suffix $(( 4*$i ))
@@ -43,19 +51,22 @@ for i in `seq $numtimes`; do
 
   scale_up_end_time=$(date +%s)
 
+  echo "Sleeping for 1 minute..."
   sleep 60
 
   scale_down_start_time=$(date +%s)
 
-  cf d -f humperdink-$suffix
-  cf d -f westley-$suffix
-  cf d -f max-$suffix
-  cf d -f buttercup-$suffix
+  echo "Deleting the apps..."
+  cf d -f humperdink-$suffix &> $suffix.out
+  cf d -f westley-$suffix &> $suffix.out
+  cf d -f max-$suffix &> $suffix.out
+  cf d -f buttercup-$suffix &> $suffix.out
 
   scale_down_end_time=$(date +%s)
 
   echo "$scale_up_start_time,$scale_up_end_time,$scale_down_start_time,$scale_down_end_time" >> results.csv
 
+  echo "Sleeping for 30 seconds..."
   sleep 30
 done
 
